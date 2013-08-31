535eb6f7910f08892ac586c834d7b0ad354a9129
Thomas Jepp <tomjepp@users.noreply.github.com>
2013-08-31 15:18:28 +0100

    Commit a horribly hacked up SRIV debug print hook based off of Rick's SRTT hook

---
+++ SaintsRowIVDebugPrintHook/code/game.cpp
@@ -0,0 +1,85 @@
+/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+#include <windows.h>
+#include <shlwapi.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "game.hpp"
+#include "patch.hpp"
+
+#define UINT32(x) (*(unsigned int *)x)
+
+typedef int (__cdecl *LUA_GETTOP)(void *);
+typedef const char * (__cdecl *LUA_TOLSTRING)(void *, int, size_t *);
+
+LUA_GETTOP lua_gettop = NULL;
+LUA_TOLSTRING lua_tolstring = NULL;
+
+int __cdecl DebugPrint(void *L)
+{
+	const char *msg;
+	
+	msg = lua_tolstring(L, 1, NULL);
+	if (_strcmpi(msg, "vint") == 0)
+	{
+		msg = lua_tolstring(L, 2, NULL);
+	}
+
+	printf("%s", msg);
+	return 0;
+}
+
+bool HookGame(void)
+{
+	printf("hooking...\n");
+
+	unsigned int debugPrintAddress = (unsigned int)&DebugPrint;
+
+	PatchCode(0x00F500D3, &debugPrintAddress, 4);
+	lua_gettop = (LUA_GETTOP)0x00FDB870;
+	lua_tolstring = (LUA_TOLSTRING)0x00FDBC60;
+
+	printf("hooked.\n");
+
+	return true;
+}
+
+bool GameAttach(void)
+{
+	AllocConsole();
+	FILE *dummy;
+	freopen_s(&dummy, "CONIN$", "rb", stdin);
+	freopen_s(&dummy, "CONOUT$", "wb", stdout);
+	freopen_s(&dummy, "CONOUT$", "wb", stderr);
+
+	printf("Allocated console.\n");
+
+	HookGame();
+
+	return true;
+}
+
+void GameDetach(void)
+{
+}

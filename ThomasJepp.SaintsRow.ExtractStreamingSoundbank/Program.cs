6ebfc8addf7fbcbfb057596f78d49663153a4eb8
Thomas Jepp <tom@tomjepp.co.uk>
2015-04-05 17:22:38 +0100

    Automatically select the right codebooks in ExtractStreamingSoundbank where possible. Provide an override if necessary too.

--- ThomasJepp.SaintsRow.ExtractStreamingSoundbank/Program.cs
+++ ThomasJepp.SaintsRow.ExtractStreamingSoundbank/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Xml;
 
 using CmdLine;
+using ThomasJepp.SaintsRow.GameInstances;
 using ThomasJepp.SaintsRow.Localization;
 using ThomasJepp.SaintsRow.Soundbanks.Streaming;
 
@@ -18,17 +19,20 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
         [CommandLineArguments(Program = "ThomasJepp.SaintsRow.ExtractStreamingSoundbank", Title = "Saints Row Streaming Soundbank Extractor", Description = "Extracts Saints Row PC Streaming Soundbanks (..._media.bnk_pc files). Supports Saints Row The Third, Saints Row IV and Saints Row Gat Out Of Hell.")]
         internal class Options
         {
-            [CommandLineParameter(Name = "soundbank", ParameterIndex = 1, Required = true, Description = "The soundbank to unpack.")]
+            [CommandLineParameter(Name = "game", ParameterIndex = 1, Required = true, Description = @"The game to load the character list files from for decoding the subtitles. Valid options are ""srgooh"", ""sriv"", ""srtt"".")]
+            public string Game { get; set; }
+
+            [CommandLineParameter(Name = "soundbank", ParameterIndex = 2, Required = true, Description = "The soundbank to unpack.")]
             public string Source { get; set; }
 
-            [CommandLineParameter(Name = "output", ParameterIndex = 2, Required = false, Description = "The location to save the extracted data. If not specified, the packfile will be extracted to a new folder with the same name in the current directory.")]
+            [CommandLineParameter(Name = "output", ParameterIndex = 3, Required = false, Description = "The location to save the extracted data. If not specified, the packfile will be extracted to a new folder with the same name in the current directory.")]
             public string Output { get; set; }
 
             [CommandLineParameter(Command = "convert", Default = true, Description = @"Convert the audio into playble OGG files. Requires ww2ogg and revorb in the same directory. Valid options are ""true"" and ""false"". Defaults to ""true"".", Name = "Convert audio")]
             public bool ConvertAudio { get; set; }
 
-            [CommandLineParameter(Command = "game", Default = "srgooh", Description = @"The game load the character list files from for decoding the subtitles. Valid options are ""srgooh"", ""sriv"", ""srtt"". Defaults to ""srgooh"".", Name = "Game")]
-            public string Game { get; set; }
+            [CommandLineParameter(Command = "codebooks", Default = null, Description = @"Override the codebooks in use by revorb. This should be a full path to a codebooks file.", Name = "Codebooks")]
+            public string Codebooks { get; set; }
         }
 
         static string ExeLocation
@@ -58,8 +62,30 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
 #endif
                 return;
             }
+
+            IGameInstance instance = GameInstance.GetFromString(options.Game);
+
             string ww2ogg = Path.Combine(ExeLocation, "ww2ogg.exe");
-            string codebooks = Path.Combine(ExeLocation, "packed_codebooks_aoTuV_603.bin");
+            string codebooks = null;
+            string codebooksPath = options.Codebooks;
+
+            if (codebooks == null)
+            {
+                switch (instance.Game)
+                {
+                    case GameSteamID.SaintsRowTheThird:
+                        codebooks = "packed_codebooks.bin";
+                        break;
+                    case GameSteamID.SaintsRowIV:
+                    case GameSteamID.SaintsRowGatOutOfHell:
+                        codebooks = "packed_codebooks_aoTuV_603.bin";
+                        break;
+                    default: throw new NotImplementedException();
+                }
+                codebooksPath = Path.Combine(ExeLocation, codebooks);
+            }
+
+
             string revorb = Path.Combine(ExeLocation, "revorb.exe");
             bool failedToFindConversionRequirements = false;
             if (options.ConvertAudio)
@@ -70,9 +96,12 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
                     failedToFindConversionRequirements = true;
                 }
 
-                if (!File.Exists(codebooks))
+                if (!File.Exists(codebooksPath))
                 {
-                    Console.WriteLine("Could not find packed_codebooks_aoTuV_603.bin at:\n{0}", codebooks);
+                    if (codebooks != null)
+                        Console.WriteLine("Could not find {0} at:\n{1}", codebooks, codebooksPath);
+                    else
+                        Console.WriteLine("Could not find your specified codebooks at:\n{1}", codebooks, codebooksPath);
                     failedToFindConversionRequirements = true;
                 }
 
@@ -86,15 +115,6 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
                 {
                     Console.WriteLine("Can't convert audio.");
                 }
-            }
-
-            GameSteamID game = GameSteamID.SaintsRowTheThird;
-
-            switch (options.Game.ToLowerInvariant())
-            {
-                case "srtt": game = GameSteamID.SaintsRowTheThird; break;
-                case "sriv": game = GameSteamID.SaintsRowIV; break;
-                case "srgooh": game = GameSteamID.SaintsRowGatOutOfHell; break;
             }
 
             using (Stream stream = File.OpenRead(options.Source))
@@ -123,6 +143,7 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
                     {
                         writer.WriteStartDocument();
                         writer.WriteStartElement("soundbank");
+                        writer.WriteAttributeString("game", instance.Game.ToString());
                         writer.WriteAttributeString("wwiseId", bnk.Header.WwiseBankId.ToString());
 
                         //writer.WriteAttributeString()
@@ -153,7 +174,7 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
                                 using (Stream metadataStream = entry.GetMetadataStream())
                                 {
                                     writer.WriteStartElement("metadata");
-                                    AudioMetadata metadata = new AudioMetadata(metadataStream, Utility.GetGamePath(game));
+                                    AudioMetadata metadata = new AudioMetadata(metadataStream, instance);
 
                                     writer.WriteAttributeString("version", metadata.Header.Version.ToString());
                                     writer.WriteAttributeString("personaid", metadata.Header.PersonaID.ToString());
@@ -220,7 +241,7 @@ namespace ThomasJepp.SaintsRow.ExtractStreamingSoundbank
                             string audioFilename = String.Format("{0}_{1:D5}.wem", bnkName, i);
                             string audioPath = Path.Combine(folderName, audioFilename);
 
-                            ProcessStartInfo ww2oggPsi = new ProcessStartInfo(ww2ogg, String.Format(@"--pcb ""{0}"" -o ""{1}"" ""{2}""", codebooks, oggPath, audioPath));
+                            ProcessStartInfo ww2oggPsi = new ProcessStartInfo(ww2ogg, String.Format(@"--pcb ""{0}"" -o ""{1}"" ""{2}""", codebooksPath, oggPath, audioPath));
                             ww2oggPsi.WindowStyle = ProcessWindowStyle.Hidden;
                             ww2oggPsi.CreateNoWindow = true;
                             Process ww2oggP = Process.Start(ww2oggPsi);

77163d5db6584ddcbfad32b904551a0bd30409f4
Thomas Jepp <tom@tomjepp.co.uk>
2016-03-17 21:42:45 +0000

    More solution platform changes, and update pack_release.cmd to cope with x64 binaries.

--- pack_release.cmd
+++ pack_release.cmd
@@ -6,10 +6,14 @@ rmdir /s /q packaging_temp 1>NUL 2>NUL
 mkdir packaging_temp 1>NUL 2>NUL
 cd packaging_temp
 mkdir ThomasJepp.SaintsRow-rev%1 1>NUL 2>NUL
+mkdir ThomasJepp.SaintsRow-rev%1\x64 1>NUL 2>NUL
 copy ..\license.txt ThomasJepp.SaintsRow-rev%1\ 1>NUL 2>NUL
 copy ..\bin\Release\*.exe ThomasJepp.SaintsRow-rev%1\ 1>NUL 2>NUL
 copy ..\bin\Release\*.dll ThomasJepp.SaintsRow-rev%1\ 1>NUL 2>NUL
 copy ..\bin\Release\*.cmd ThomasJepp.SaintsRow-rev%1\ 1>NUL 2>NUL
+copy ..\bin\x64\Release\*.exe ThomasJepp.SaintsRow-rev%1\x64\ 1>NUL 2>NUL
+copy ..\bin\x64\Release\*.dll ThomasJepp.SaintsRow-rev%1\x64\ 1>NUL 2>NUL
+copy ..\bin\x64\Release\*.cmd ThomasJepp.SaintsRow-rev%1\x64\ 1>NUL 2>NUL
 mkdir ThomasJepp.SaintsRow-rev%1\external 1>NUL 2>NUL
 copy ..\bin\Release\external\*.* ThomasJepp.SaintsRow-rev%1\external\ 1>NUL 2>NUL
 "C:\Program Files\7-Zip\7z.exe" a ..\ThomasJepp.SaintsRow-rev%1.zip ThomasJepp.SaintsRow-rev%1

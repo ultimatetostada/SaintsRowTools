57d0393ca7eb142d4f01c748498beac131deb819
Thomas Jepp <tom@tomjepp.co.uk>
2017-04-30 14:45:48 +0100

    Fix ExtractStrings for SR2, add an option to skip loading XTBLs, and show the progress of loading XTBL files.

--- ThomasJepp.SaintsRow.ExtractStrings/Program.cs
+++ ThomasJepp.SaintsRow.ExtractStrings/Program.cs
@@ -25,6 +25,9 @@ namespace ThomasJepp.SaintsRow.ExtractStrings
 
             [CommandLineParameter(Name = "output", ParameterIndex = 3, Required = false, Description = "The output file to create. If not specified, the input filename will be used with the exension changed to \".xml\".")]
             public string Output { get; set; }
+
+            [CommandLineParameter(Command = "load_xtbls", Default = true, Required = false, Description = "Should XTBLs be loaded? Defaults to 'true'")]
+            public bool LoadXtbls { get; set; }
         }
 
         public static void Main(string[] args)
@@ -53,22 +56,30 @@ namespace ThomasJepp.SaintsRow.ExtractStrings
             string languageCode = filename.Remove(0, filename.Length - 2);
             Language language = LanguageUtility.GetLanguageFromCode(languageCode);
 
-            Console.WriteLine("Loading XTBL files...");
             Dictionary<UInt32, string> hashLookup = new Dictionary<UInt32, string>();
-            var results = instance.SearchForFiles("*.xtbl");
-            foreach (var pair in results)
+
+            if (options.LoadXtbls)
             {
-                string xtbl = null;
-                using (StreamReader reader = new StreamReader(pair.Value.GetStream()))
+                Console.WriteLine("Loading XTBL files...");
+                Dictionary<string, FileSearchResult> results = instance.SearchForFiles("*.xtbl");
+                int i = 0;
+                foreach (var pair in results)
                 {
-                    xtbl = reader.ReadToEnd();
-                }
-                Regex regex = new Regex("<Name>(.*?)</Name>", RegexOptions.Compiled);
-                foreach (Match m in regex.Matches(xtbl))
-                {
-                    uint hash = Hashes.CrcVolition(m.Groups[1].Value);
-                    if (!hashLookup.ContainsKey(hash))
-                        hashLookup.Add(hash, m.Groups[1].Value);
+                    i++;
+                    Console.WriteLine("[{0}/{1}] Loading xtbl: {2}", i, results.Count, pair.Key);
+
+                    string xtbl = null;
+                    using (StreamReader reader = new StreamReader(pair.Value.GetStream()))
+                    {
+                        xtbl = reader.ReadToEnd();
+                    }
+                    Regex regex = new Regex("<Name>(.*?)</Name>", RegexOptions.Compiled);
+                    foreach (Match m in regex.Matches(xtbl))
+                    {
+                        uint hash = Hashes.CrcVolition(m.Groups[1].Value);
+                        if (!hashLookup.ContainsKey(hash))
+                            hashLookup.Add(hash, m.Groups[1].Value);
+                    }
                 }
             }
 
